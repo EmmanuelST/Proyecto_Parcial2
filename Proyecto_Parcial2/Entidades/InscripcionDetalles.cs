11d9f7bf2c49ce99c08a96263a3b17213360a66a
Emmanuel Suarez Taveras <manuel.413@hotmail.com>
2019-06-26 11:07:06 -0400

    Agregando InscripcionDetalles

---
+++ Proyecto_Parcial2/Entidades/InscripcionDetalles.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Parcial2.Entidades
+{
+    public class InscripcionDetalles
+    {
+        public int InscripcionDetallesId { get; set; }
+        public int InscripcionId { get; set; }
+        public int AsignaturaId { get; set; }
+        public decimal SubTotal { get; set; }
+
+        public InscripcionDetalles()
+        {
+            InscripcionDetallesId = 0;
+            InscripcionId = 0;
+            AsignaturaId = 0;
+            SubTotal = 0;
+        }
+    }
+}
