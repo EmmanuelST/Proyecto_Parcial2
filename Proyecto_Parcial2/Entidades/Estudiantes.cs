4469dd30d265f41ed938a00f416662c39a92f310
Emmanuel Suarez Taveras <manuel.413@hotmail.com>
2019-06-26 10:56:21 -0400

    Agregando la entidad Estudiantes

---
+++ Proyecto_Parcial2/Entidades/Estudiantes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Parcial2.Entidades
+{
+    public class Estudiantes
+    {
+        public int EstudianteId { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public string Nombres { get; set; }
+        public decimal Balance { get; set; }
+
+        public Estudiantes()
+        {
+            EstudianteId = 0;
+            FechaIngreso = DateTime.Now;
+            Nombres = string.Empty;
+            Balance = 0;
+        }
+    }
+}
