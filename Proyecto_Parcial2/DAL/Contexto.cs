1dcf428ff634190b4fdbda2b0cf1ccacec628c66
Emmanuel Suarez Taveras <manuel.413@hotmail.com>
2019-06-26 11:15:43 -0400

    Agregando la clase Contexto

---
+++ Proyecto_Parcial2/DAL/Contexto.cs
@@ -0,0 +1,22 @@
+﻿using Proyecto_Parcial2.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Parcial2.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Estudiantes>Estudiante { get; set; }
+        public DbSet<Asignaturas>Asignatura { get; set; }
+        public DbSet<Inscripcion>Inscripcion { get; set; }
+
+        public Contexto() : base("Constr")
+        {
+
+        }
+    }
+}
