2131e7bafd05ff1b2f71ca7f4dbe64267a8fc999
Emmanuel Suarez Taveras <manuel.413@hotmail.com>
2019-06-26 11:00:19 -0400

    Agregando la entidad Inscripcion

---
+++ Proyecto_Parcial2/Entidades/Inscripcion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Parcial2.Entidades
+{
+    public class Inscripcion
+    {
+        public int InscripcionId { get; set; }
+        public DateTime Fecha { get; set; }
+        public decimal Monto { get; set; }
+
+        public Inscripcion()
+        {
+            InscripcionId = 0;
+            Fecha = DateTime.Now;
+            Monto = 0;
+        }
+    }
+}
